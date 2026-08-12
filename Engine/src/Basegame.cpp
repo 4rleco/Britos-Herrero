0e3d6b276b9137f2301aaec3708a1f1a81c80e62
4rleco <117652001+4rleco@users.noreply.github.com>
2026-08-12 14:02:01 -0300

    Done basic glfw implementation

---
+++ Engine/src/Basegame.cpp
@@ -0,0 +1,35 @@
+#include <GLFW/glfw3.h>
+
+#include "Basegame.h"
+
+int BaseGame::RunEngine()
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ Engine/src/Basegame.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "EngineAPI.h"
+
+ENGINE_API class BaseGame
+{
+public:
+	ENGINE_API int RunEngine();
+};
