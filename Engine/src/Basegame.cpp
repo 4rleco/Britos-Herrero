3f9d8292ad73520cb59d00080ef2d3aa85b55326
4rleco <117652001+4rleco@users.noreply.github.com>
2026-08-26 12:31:29 -0300

    Modify base game

--- Engine/src/Basegame.cpp
+++ Engine/src/Basegame.cpp
@@ -3,38 +3,32 @@
 #include "Renderer/Renderer.h"
 #include "Window/Window .h"
 
+#include "Entity/Shape/Triangle.h"
+
 int BaseGame::RunEngine(int width, int height, const char* title)
 {
 	Window window;
 
 	window.InitWindow(width, height, title);
 
-	float vertices[6] = {
-		-0.5f, -0.5f,
-		0.0f, 0.5f,
-		0.5f, -0.5f
-	};
+	Triangle triangle = Triangle(0.0f, 0.0f, 0.0f, 0.5f, 0.5f);
 
 	Renderer::GetInstance().SetWindowContext(window.GetWindow());
 
 	Renderer::GetInstance().CheckGlewStatus();
 
-	Renderer::GetInstance().BindBuffers(vertices);
+	Renderer::GetInstance().BindBuffers();
 
 	while(!window.ShouldClose())
 	{
-		std::cout << "entra" << std::endl;
-
 		window.Clear();
 
-		Renderer::GetInstance().Draw();
+		triangle.Draw();
 
 		Renderer::GetInstance().UpdateBuffers(window.GetWindow());
 	}
 
 	Renderer::GetInstance().CleanData(window.GetWindow());
-
-	std::cout << "sale" << std::endl;
 
 	return 0;
 }
